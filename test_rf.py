7e733003073735936838cfe2e036af37d25e4487
GvKosinberg <666kosyak666@gmail.com>
2017-12-28 10:03:09 +0300

    Strange bug fixo

--- test_rf.py
+++ test_rf.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 #Author: Antipin S.O. @RLDA
-# import os, sys
-# import rfm69
-# import logging
-# import time
+import os, sys
+import rfm69
+import logging
+import time
 import random
-# import paho.mqtt.publish as pbl
+import paho.mqtt.publish as pbl
 
 """
 	Инициализация логгера, RFM69
@@ -136,9 +136,8 @@ def read_fake(rfm):
 	print("Done")
 
 if __name__ == "__main__":
-	print(get_random_state("OO"))
-	# rfm = init()
-	# while True:
-	# 	read_fake(rfm)
-	# 	time.sleep(5)
-	# print("That's all, folks!")
+	rfm = init()
+	while True:
+		read_fake(rfm)
+		time.sleep(5)
+	print("That's all, folks!")
