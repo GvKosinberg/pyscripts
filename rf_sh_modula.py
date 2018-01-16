447291acc6a58a860bee4ceca6d0fe32a7e0bf01
GvKosinberg <666kosyak666@gmail.com>
2018-01-16 09:37:56 +0300

    Try

--- rf_sh_modula.py
+++ rf_sh_modula.py
@@ -8,8 +8,8 @@
 import os, sys
 
 #TODO: uncomment 4 realz
-#import rfm69
-#import paho.mqtt.publish as mqpb
+import rfm69
+import paho.mqtt.publish as mqpb
 
 import time
 
@@ -28,12 +28,12 @@ logging.basicConfig(level=logging.DEBUG)
 """
 def init_rfm():
     #TEMP: faek rfm unit
-    rfm_unit = "sum_sh1et"
+    #rfm_unit = "sum_sh1et"
     ##_____##
-	# myconf = rfm69.RFM69Configuration()
-	# rfm_unit = rfm69.RFM69(dio0_pin=24, reset_pin=22, spi_channel = 0, config = myconf)
-	# #setting RSSI treshold
-	# rfm_unit.set_rssi_threshold(-114)
+	myconf = rfm69.RFM69Configuration()
+	rfm_unit = rfm69.RFM69(dio0_pin=24, reset_pin=22, spi_channel = 0, config = myconf)
+	#setting RSSI treshold
+	rfm_unit.set_rssi_threshold(-114)
     return rfm_unit
 
 """
@@ -43,13 +43,14 @@ class Device:
     def __init__(self, d_type, name, rfm, d_timeout):
         #Тип устройства (датчик/исполнитель) (str)
         __types = [#-------------------------------------------#
-                    "SNC_T_AIR", "SNC_T_WAT", "SNC_T_HTR", "SNC_LUM", "SNC_HUM",
-                    "SNC_DOOR",  "SNC_WARN_LEAK", "SNC_WARN_FLAME",
-                    "SNC_WARN_SMOKE", "SNC_PRES_PRES", "SNC_PRES_MOT", "FAKE",
+                    "sncs/temp/air", "sncs/temp/water", "sncs/temp/heater",
+                    "sncs/lumi", "sncs/humi", "sncs/doors",
+                    "warn/leak", "warn/smoke", "warn/smoke",
+                    "pres/pres", "pres/motion", "FAKE",
                     #------------------------------------------#
-                    "CNTR",
+                    "cntrs",
                     #------------------------------------------#
-                    "CNTRL_RELAY", "CNRL_DIM"]
+                    "devices/relays", "devices/dimmers"]
 
         try:
             if (__types.index(d_type) != None):
@@ -89,12 +90,15 @@ class Device:
         Метод записи полученного значения датчика в брокер
     """
     def write2mqtt(self):
-        #mqtt_topic = "oh" + self.d_type + self.name
+        mqtt_topic = "oh/" + self.d_type + self.name
+
+        #TEMP: random data
         self.data = self.get_random_state()
+
         mqtt_val = self.data
-        #mqtt_val = self.data
-        #pbl.single(mqtt_topic, mqtt_val, hostname="localhost", port=1883)
-        print('Obj: %s ' %(self.d_type +"/"+ self.name))
+        pbl.single(mqtt_topic, mqtt_val, hostname="localhost", port=1883)
+
+        #print('Obj: %s ' %(self.d_type +"/"+ self.name))
         #print('Last responce: %s' %str(self.last_responce))
 
         print(mqtt_val)
@@ -107,19 +111,19 @@ class Device:
 
     #TEMP: random generator for tests
     def get_random_state(self):
-        __val_limits = {"SNC_T_AIR": [19.00, 25.00],
-                        "SNC_T_WAT": [5.00, 22.00],
-                        "SNC_T_HTR": [50.00, 100.00],
-                        "SNC_LUM": [150.00, 300.00],
-                        "SNC_HUM": [0, 100],
+        __val_limits = {"sncs/temp/air": [19.00, 25.00],
+                        "sncs/temp/water": [5.00, 22.00],
+                        "sncs/temp/heater": [50.00, 100.00],
+                        "sncs/lumi": [150.00, 300.00],
+                        "sncs/humi": [0, 100],
                         "FAKE": [0, 3378]
                     }
-        __defas = {"SNC_DOOR": ["OPEN", "CLOSED"],
-                 "SNC_PRES_MOT": ["ON", "OFF"],
-                 "SNC_PRES_PRES": ["ON", "OFF"],
-                 "SNC_WARN_LEAK": ["HIGH", "LOW"],
-                 "SNC_WARN_FLAME": ["HIGH", "LOW"],
-                 "SNC_WARN_SMOKE": ["HIGH", "LOW"]}
+        __defas = {"sncs/doors": ["OPEN", "CLOSED"],
+                 "pres/pres": ["ON", "OFF"],
+                 "pres/motion": ["ON", "OFF"],
+                 "warn/leak": ["HIGH", "LOW"],
+                 "warn/smoke": ["HIGH", "LOW"],
+                 "warn/flame": ["HIGH", "LOW"]}
         if __defas.get(self.d_type) != None:
             state = random.randint(0,1)
             out = __defas[self.d_type][state]
@@ -133,19 +137,46 @@ class Device:
 if __name__ == "__main__":
     rfm = init_rfm()
     try:
-        fake_snc_1 = Device("SNC_T_AIR", "1", rfm)
-        fake_snc_2 = Device("FAKE", "2", rfm)
-        fake_snc_3 = Device("SNC_PRES_PRES", "1", rfm)
+        fake_t_air = Device("sncs/temp/air", "1", rfm, 5)
+        fake_t_wat = Device("sncs/temp/wat", "1", rfm, 5)
+        fake_t_heat = Device("sncs/temp/heat", "1", rfm, 5)
+
+        fake_humi = Device("sncs/humi", "1", rfm, 5)
+        fake_lumi = Device("sncs/humi", "1", rfm, 5)
+
+        fake_door = Device("sncs/doors", "1", rfm, 5)
+
+        fake_leak = Device("warn/leak", "1", rfm, 5)
+        fake_smoke = Device("warn/smoke", "1", rfm, 5)
+        fake_flame = Device("warn/flame", "1", rfm, 5)
+
+        fake_pres = Device("pres/pres", "1", rfm, 5)
+        fake_mot = Device("pres/motion", "1", rfm, 5)
+
+        fake_cntr = Device("cntrs", "1", rfm, 5)
     except Exception as e:
         print("Init fux")
         raise(e)
 
     try:
         while(True):
-            fake_snc_1.write2mqtt()
-            fake_snc_2.write2mqtt()
-            fake_snc_3.write2mqtt()
+            fake_t_air.write2mqtt()
+            fake_t_wat.write2mqtt()
+            fake_t_heat.write2mqtt()
+
+            fake_humi.write2mqtt()
+            fake_lumi.write2mqtt()
+
+            fake_door.write2mqtt()
+
+            fake_leak.write2mqtt()
+            fake_smoke.write2mqtt()
+            fake_flame.write2mqtt()
+
+            fake_pres.write2mqtt()
+            fake_mot.write2mqtt()
+
+            fake_cntr.write2mqtt()
             time.sleep(5)
-            fake_snc_3.check_timeout()
     except KeyboardInterrupt:
         print("That's all, folks")
