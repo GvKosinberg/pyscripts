37845d89b6bcda81426e6f0006692537ac09b826
GvKosiberg <666kosyak666@gmail.com>
2018-01-31 14:09:29 +0300

    multiline. attempt#2

--- rfm_stuff/rf_sh_modula.py
+++ rfm_stuff/rf_sh_modula.py
@@ -202,8 +202,8 @@ class Sencor:
         __t_diff = time.time() - self.last_responce
         if __t_diff > self.d_timeout:
             self.data = 'Время ответа истекло. \n' +
-                        'Последнее значение:' + self.data + '\n'
-                        'Последний отклик:' + self.last_responce
+                'Последнее значение:' + self.data + '\n' + 
+                'Последний отклик:' + self.last_responce
             log.debug("Sencor: %s: time between responces: %s" % (
                     (self.d_type+":"+self.name), __t_diff))
 
