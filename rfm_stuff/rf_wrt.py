9e6ad4b86bf96d1022a05081a964ab3a2c7e32b4
GvKosinberg <666kosyak666@gmail.com>
2018-02-13 10:42:47 +0300

    relay_test

---
+++ rfm_stuff/rf_wrt.py
@@ -0,0 +1,30 @@
+import rfm69
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger("testo")
+
+def init_rfm():
+    myconf = rfm69.RFM69Configuration()
+    rfm_unit = rfm69.RFM69(
+                            dio0_pin=24,
+                            reset_pin=22,
+                            spi_channel=0,
+                            config=myconf)
+    # setting RSSI treshold
+    rfm_unit.set_rssi_threshold(-114)
+    return rfm_unit
+
+def write_true():
+    pack = [210, 4, 0, 14, 0, 1]
+    # pack[0] = 210
+    # pack[1] =
+    rfm.send_packet(pack)
+
+
+if __name__ == '__main__':
+    rfm = init_rfm()
+    try:
+        write_true()
+    except KeyboardInterrupt:
+        print("That's all, folks")
